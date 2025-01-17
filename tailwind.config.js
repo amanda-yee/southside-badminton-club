e007c1196a372459977563dd2b6ea1f2a2b16ac0
Amanda Yee <amandakyee@gmail.com>
2025-01-17 14:32:14 -0500

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all source files
+  ],
+  theme: {
+    extend: {}, // Customize the default theme here if needed
+  },
+  plugins: [], // Add plugins here if needed
+};
+
+
