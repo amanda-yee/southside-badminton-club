37d9e08fcd0b17d067dfc086b22ab147827e40fc
Amanda Yee <amandakyee@gmail.com>
2025-01-17 14:44:16 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { GlobeIcon } from './components/Icons';
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-6 text-center">
-      <h1 className="text-3xl font-bold">Test ailwind CSS</h1>
+    <div>
+      <header className="grid grid-rows-[20px_1fr_20px] items-center justify-items-left gap-4 sm:p-12 sm:pb-0 font-[family-name:var(--font-geist-sans)]">
+        <h1 className="flex items-center gap-2 font-bold">
+            Southside Badminton Club Gold Coast
+            <GlobeIcon />
+        </h1>
+      </header>
     </div>
   );
 }
