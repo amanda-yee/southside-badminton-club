585090db33e817d5d25ca597bb7d2bd3351effb8
Amanda Yee <amandakyee@gmail.com>
2025-01-19 23:26:29 -0500

    add book now to footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
-import { FacebookIcon, PhoneIcon, EmailIcon, PinIcon } from './components/Icons';
+import { FacebookIcon, PhoneIcon, EmailIcon, PinIcon, DiagonalArrowIcon } from './components/Icons';
 
 function App() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] min-h-screen">
-      <header className="grid grid-rows-[20px_1fr_20px] items-center justify-items-left gap-4 sm:p-12 sm:pb-0 font-[family-name:var(--font-geist-sans)]">
-        <h1 className="flex items-center gap-2 font-bold">
-            Southside Badminton Club Gold Coast
-        </h1>
-        <p className="italic">
+        <header className="grid grid-rows-[20px_1fr_20px] items-center justify-items-left gap-4 sm:p-12 sm:pb-0 font-[family-name:var(--font-geist-sans)]">
+          <h1 className="text-2xl flex items-center gap-2 font-bold">
+              Southside Badminton Club Gold Coast
+             
+          </h1>
+        <p>
           We are a non-profit social badminton club. All levels of players are welcome. Come and join us for fun, fitness and meeting friends!
         </p>
 
-        <a className="absolute top-5 right-5 bg-[#5be0e6] font-bold py-1 px-3 rounded hover:text-[#FFFFFF]" 
+        <a className="absolute top-5 right-5 bg-[#5be0e6] inline-flex gap-2 font-bold py-2 px-3 rounded hover:text-[#FFFFFF]" 
            href="https://events.humanitix.com/southside-badminton-club-session-booking"
            target="_blank"
           >
-          BOOK NOW
+          BOOK NOW <DiagonalArrowIcon />
         </a>
       </header>
 
@@ -36,19 +37,26 @@ function App() {
         <div>
           <p className="font-bold">Contact Us:</p>
           <ul >
-            <li className="flex items-center gap-2"><PhoneIcon />+61419002098</li>
+            <li className="flex items-center gap-2"><PhoneIcon />+61 419 002 098</li>
             <li className="flex items-center gap-2"><EmailIcon /> southsidebadmintonclub@gmail.com</li>
             <li className="flex items-center gap-2"><PinIcon />Lot 22 Assembly Drive, Varsity Lakes QLD 4227, Australia</li>
           </ul>
         </div>
         {/* items-end push to right, justify-end pushes to bottom */}
-        <div className="text-right flex flex-col gap-3 items-end justify-end"> 
-          <a
-            href="https://www.facebook.com/p/Southside-Badminton-Club-Gold-Coast-100033686513550/"
-            target="_blank"
+        <div className="text-right flex flex-col gap-3 items-end justify-end ">
+          <div className="flex items-center gap-10">
+            <a className="inline-flex font-bold gap-2 py-1 px-3 rounded hover:text-[#FFFFFF]" 
+                href="https://events.humanitix.com/southside-badminton-club-session-booking"
+                target="_blank"
             >
-            <FacebookIcon />
-          </a>
+              BOOK NOW <DiagonalArrowIcon />
+            </a>
+
+            <a href="https://www.facebook.com/p/Southside-Badminton-Club-Gold-Coast-100033686513550/" target="_blank">
+              <FacebookIcon />
+            </a> 
+          </div>
+          
           <span>&copy; Azzurra Studios 2025</span>
         </div>
       </footer>
--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,10 +1,3 @@
-// export const GlobeIcon = () => (
-//     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-//         <path d="M15.75 8.25a.75.75 0 0 1 .75.75c0 1.12-.492 2.126-1.27 2.812a.75.75 0 1 1-.992-1.124A2.243 2.243 0 0 0 15 9a.75.75 0 0 1 .75-.75Z" />
-//         <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM4.575 15.6a8.25 8.25 0 0 0 9.348 4.425 1.966 1.966 0 0 0-1.84-1.275.983.983 0 0 1-.97-.822l-.073-.437c-.094-.565.25-1.11.8-1.267l.99-.282c.427-.123.783-.418.982-.816l.036-.073a1.453 1.453 0 0 1 2.328-.377L16.5 15h.628a2.25 2.25 0 0 1 1.983 1.186 8.25 8.25 0 0 0-6.345-12.4c.044.262.18.503.389.676l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 0 1-1.161.886l-.143.048a1.107 1.107 0 0 0-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 0 1-1.652.928l-.679-.906a1.125 1.125 0 0 0-1.906.172L4.575 15.6Z" clip-rule="evenodd" />
-//     </svg>
-// );
-
 // fill-current means the SVG fill color inherits from the current text color (i.e. what the footer text color is)
 export const FacebookIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 50 50" className="fill-current size-10"> 
@@ -33,4 +26,11 @@ export const PinIcon = () => (
         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
     </svg>
 );
-  
+
+
+export const DiagonalArrowIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+    </svg>
+);
+
