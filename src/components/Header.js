345bcec29a5d66d96125ba29f926f355b563a63e
Amanda Yee <amandakyee@gmail.com>
2025-02-02 15:11:37 -0500

    clean up css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { DiagonalArrowIcon } from './Icons';
 
 const Header = () => {
     return (
-        <header className="flex items-start gap-5 border-b-2 border-[#000000] sm:p-12 sm:pb-0 font-[family-name:var(--font-geist-sans)]">
+        <header className="flex items-center gap-5 border-b-2 border-[#000000] sm:p-12 sm:pb-0 font-[family-name:var(--font-geist-sans)]">
 
           <img src={`${process.env.PUBLIC_URL}/images/southside-logo.png`} 
                alt="Southside Badminton Club Logo" 
